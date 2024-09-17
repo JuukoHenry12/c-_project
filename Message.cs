1a9ea1a57a855270687c0f931b32e36b883bd06a
JuukoHenry12 <juukohenry23@gmail.com>
2024-09-17 05:49:07 -0400

    update

---
+++ Message.cs
@@ -0,0 +1,21 @@
+using System;
+namespace Messages
+
+{
+    
+    class Message{
+         
+        public static void Hello(){
+          Console.WriteLine("Hello ! Welcome to the program");
+        }
+
+        public static  void Waiting(){
+            Console.WriteLine("I'am waiting for somethings");
+        }
+
+      public static void Byte(){
+         Console.WriteLine("Bye ! Thanks for visting");
+       } 
+    }
+    
+}
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Messages;
+namespace App
+{
+    class HelloWorld
+    {
+        public static void Main(String[] args)
+        {
+          Message.Hello();
+          Message.Waiting();
+          Message.Byte();
+        }
+   }
+
+}
